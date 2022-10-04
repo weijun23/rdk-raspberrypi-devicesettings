8741d2c41e06911832b0d82e7b5ab1a3b19f0ea3
Anil Vadla <anil_vadla@comcast.com>
2022-10-04 14:12:51 +0000

    REFPLTV-1397: SPDIF port Enable/Disable not supported on RPI platform.
    Reason for change: Removed SPDIF port related definitions from dsAudioSettings.h as not supported on RPI platform.
    
    Test Procedure: Get supported Audio ports should not show SPDIF in the list.
    
    Risks: Low
    
    Signed-off-by: avadla226 <Anil_Vadla@comcast.com>
    Change-Id: Idcf2992e0638d73e834b74686f1380090de1f439

--- dsAudioSettings.h
+++ dsAudioSettings.h
@@ -34,13 +34,10 @@ namespace  {
 /*
  * Setup the supported configurations here.
  */
-static const dsAudioPortType_t 			kSupportedPortTypes[] 				= { dsAUDIOPORT_TYPE_HDMI, dsAUDIOPORT_TYPE_SPDIF };
+static const dsAudioPortType_t 			kSupportedPortTypes[] 				= { dsAUDIOPORT_TYPE_HDMI };
 static const dsAudioEncoding_t 		kSupportedHDMIEncodings[]			= { dsAUDIO_ENC_PCM, dsAUDIO_ENC_AC3};
 static const dsAudioCompression_t 	kSupportedHDMICompressions[] 		= { dsAUDIO_CMP_NONE, dsAUDIO_CMP_LIGHT, dsAUDIO_CMP_MEDIUM, dsAUDIO_CMP_HEAVY, };
 static const dsAudioStereoMode_t 	kSupportedHDMIStereoModes[] 		= { dsAUDIO_STEREO_STEREO, dsAUDIO_STEREO_SURROUND, };
-static const dsAudioEncoding_t 		kSupportedSPDIFEncodings[] 			= { dsAUDIO_ENC_PCM, dsAUDIO_ENC_AC3, };
-static const dsAudioCompression_t 	kSupportedSPDIFCompressions[] 		= { dsAUDIO_CMP_NONE, dsAUDIO_CMP_LIGHT, dsAUDIO_CMP_MEDIUM, dsAUDIO_CMP_HEAVY, };
-static const dsAudioStereoMode_t 	kSupportedSPDIFStereoModes[] 		= { dsAUDIO_STEREO_STEREO, dsAUDIO_STEREO_SURROUND, };
 
 static const dsAudioTypeConfig_t 	kConfigs[]= {
 		{
@@ -52,16 +49,6 @@ static const dsAudioTypeConfig_t 	kConfigs[]= {
 		/*.encodings = */				kSupportedHDMIEncodings,
 		/*.numSupportedStereoModes = */	dsUTL_DIM(kSupportedHDMIStereoModes),
 		/*.stereoModes = */				kSupportedHDMIStereoModes,
-		},
-		{
-		/*.typeId = */					dsAUDIOPORT_TYPE_SPDIF,
-		/*.name = */					"SPDIF", //SPDIF
-		/*.numSupportedCompressions = */dsUTL_DIM(kSupportedSPDIFCompressions),
-		/*.compressions = */			kSupportedSPDIFCompressions,
-		/*.numSupportedEncodings = */	dsUTL_DIM(kSupportedSPDIFEncodings),
-		/*.encodings = */				kSupportedSPDIFEncodings,
-		/*.numSupportedStereoModes = */	dsUTL_DIM(kSupportedSPDIFStereoModes),
-		/*.stereoModes = */				kSupportedSPDIFStereoModes,
 		}
 };
 
@@ -71,21 +58,14 @@ static const dsVideoPortPortId_t connectedVOPs[dsAUDIOPORT_TYPE_MAX][dsVIDEOPORT
 		},
 		{/*VOPs connected to HDMI Audio */
 				{dsVIDEOPORT_TYPE_HDMI, 0},
-		},
-		{/*VOPs connected to SPDIF Audio */
-				{dsVIDEOPORT_TYPE_HDMI, 0},
-		},
+		}
 };
 
 static const dsAudioPortConfig_t kPorts[] = {
 		{
 		/*.typeId = */ 					{dsAUDIOPORT_TYPE_HDMI, 0},
 		/*.connectedVOPs = */			connectedVOPs[dsAUDIOPORT_TYPE_HDMI],
-		},
-		{
-		/*.typeId = */ 					{dsAUDIOPORT_TYPE_SPDIF, 0},
-		/*.connectedVOPs = */			connectedVOPs[dsAUDIOPORT_TYPE_SPDIF],
-		},
+		}
 };
 
 }
